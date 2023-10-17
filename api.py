700184bc8ea77cbcab0df4230f9ddcc66b6d204c
Lucas R. Domingues <lucasrdomingues-lrd@hotmail.com>
2023-10-17 16:46:06 -0300

    Added basic char to work char route

--- api.py
+++ api.py
@@ -12,20 +12,31 @@ link = "https://api.openai.com/v1/chat/completions"
 model_id = "gpt-3.5-turbo"
 
 def acessGPT(objectName):
-    # rpgContent = {
-    #     'character': f"Personagem {objectName}",
-    #     'attrs': 'Atributos',
-    #     'background': 'Historia generica de background'
-    # }
-    message_body = {
-        "model": model_id,
-        "messages": [{"role": "user", "content": buildMessage(objectName)}] 
+    message = {
+        "character": {
+            "name": "Carlos, o Valente",
+            "class": "Guerreiro",
+            "region": "Artalúdico",
+        },
+        "attrs": {
+            "hp": "100", # pontos de vida
+            "str": "10", # forca
+            "dex": "10", # destreza
+            "car": "10", # carisma
+            "per": "10", # percepcao
+            "wis": "10", # inteligencia/sabedoria
+            "con": "10", # constituicao
+        },
+        "background": "Carlos, o Valente, e um Guerreiro habil com espada longa e preciso nos seus tiros de arco e flecha"
     }
-    message_body = json.dumps(message_body)
+    # message_body = {
+    #     "model": model_id,
+    #     "messages": [{"role": "user", "content": buildMessage(objectName)}] 
+    # }
+    # message_body = json.dumps(message_body)
 
-    request = req.post(link, headers=headers, data=message_body)
-    ans = request.json()
+    # request = req.post(link, headers=headers, data=message_body)
+    # ans = request.json()
 
-    message = ans["choices"][0]["message"]["content"]
-    # return rpgContent
+    # message = ans["choices"][0]["message"]["content"]
     return message
