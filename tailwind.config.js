e31b9545b136b4ffdbdb558fc93deb5120c8c39f
Lucas R. Domingues <lucasrdomingues-lrd@hotmail.com>
2023-10-19 12:05:00 -0300

    New tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./templates/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ras-color': '#862632'
+      },
+      width: {
+        '120px': '120px',
+        '300px': '300px',
+        '256px': '256px',
+        '128px': '128px',
+      }
+    },
   },
   plugins: [],
 }
