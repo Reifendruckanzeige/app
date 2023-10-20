caf3077dbe9012f2e89e04c4229c626628f4ddfb
Lucas R. Domingues <lucasrdomingues-lrd@hotmail.com>
2023-10-20 12:24:35 -0300

    fixed

--- api/api.py
+++ api/api.py
@@ -11,12 +11,14 @@ headers = {"Authorization": f"Bearer {os.environ['OPENAI_TOKEN']}", "Content-Typ
 link = "https://api.openai.com/v1/chat/completions"
 model_id = "gpt-3.5-turbo"
 
-def generateImage(text):
+def generateImage(text, objectName):
     endpoint = "https://api.openai.com/v1/images/generations"
     resolution = "256x256"
 
+    prompt = text if text else objectName
+    
     message_body = {
-        "prompt": text,
+        "prompt": prompt,
         "size": resolution,
     }
     message_body = json.dumps(message_body)
@@ -43,20 +45,20 @@ def accessGPT(objectName, charLevel):
     background = messageFormatted[12].split(": ")[1]
 
     return {
-        "name": messageFormatted[0].split(": ")[1],
-        "alignment": messageFormatted[1].split(": ")[1],
-        "class": messageFormatted[2].split(": ")[1],
-        "race": messageFormatted[3].split(": ")[1],
-        "skill": messageFormatted[4].split(": ")[1],
-        "weapon": messageFormatted[11].split(": ")[1],
+        "name": messageFormatted[0].split(": ")[1] if messageFormatted[0] else None,
+        "alignment": messageFormatted[1].split(": ")[1] if messageFormatted[1] else None,
+        "class": messageFormatted[2].split(": ")[1] if messageFormatted[2] else None,
+        "race": messageFormatted[3].split(": ")[1] if messageFormatted[3] else None,
+        "skill": messageFormatted[4].split(": ")[1] if messageFormatted[4] else None,
+        "weapon": messageFormatted[11].split(": ")[1] if messageFormatted[11] else None,
         "attributes": {
-            "hp": messageFormatted[5].split(": ")[1], 
-            "str": messageFormatted[6].split(": ")[1],
-            "dex": messageFormatted[7].split(": ")[1],
-            "car": messageFormatted[8].split(": ")[1],
-            "per": messageFormatted[9].split(": ")[1],
-            "wis": messageFormatted[10].split(": ")[1],
+            "hp": messageFormatted[5].split(": ")[1] if messageFormatted[5] else None, 
+            "str": messageFormatted[6].split(": ")[1] if messageFormatted[6] else None,
+            "dex": messageFormatted[7].split(": ")[1] if messageFormatted[7] else None,
+            "car": messageFormatted[8].split(": ")[1] if messageFormatted[8] else None,
+            "per": messageFormatted[9].split(": ")[1] if messageFormatted[9] else None,
+            "wis": messageFormatted[10].split(": ")[1] if messageFormatted[10] else None,
         },
         "background": background,
-        "imageUrl": generateImage(background)
+        "imageUrl": generateImage(background, objectName)
     }
